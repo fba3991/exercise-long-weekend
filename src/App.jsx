9906971e767d94115d70b7379c82781bcf178788
Fabioes <fabionike@icloud.com>
2023-12-11 01:33:10 +0100

    feat

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,9 @@ import CustomList from "./componnets/CutomList";
 import "./App.css";
 import { useState } from "react";
 
-const listaOrdinata = ["alessio", "laura", "maks"];
-  const listaNonOrdinata = ["Alex", "laura", "maks"];
-  const allList = [
-    {
-      title: "Invitati",
-      type: "ul",
-      list: ["Tizio", "Caio"],
-    },
-
-    {
-      title: "Invitati",
-      type: "ul",
-      list: ["Tizio", "Caio"],
-    },
+function App() {
+  // Definizione dell'array di oggetti allLists
+  const allLists = [
     {
       title: "Invitati",
       type: "ul",
@@ -27,40 +16,69 @@ const listaOrdinata = ["alessio", "laura", "maks"];
       list: ["Tizio", "Caio"],
     },
   ];
-function App() {
-  
 
-  const [listParty, setListParty] =useState(allList)
+  const [listaSeparata, setListaSeparata] = useState([]);
+  const [inputValue, setInputValue] = useState(""); /* gestione dell input */
+  const [msg, setMsg] = useState(""); /*Stato per il messaggio di benvenuto
+  */
 
-
+  const SalutaInvitato = (invitati) => {
+    setMsg(<> ciao {invitati}, sei il benvenuto</>);
+    /* Funzione che imposta il messaggio di benvenuto per un invitato specifico */
+  };
 
   return (
     <div>
-       <CustomList type="ol" list={listaOrdinata} />
-      <CustomList type="ul" list={listaNonOrdinata} />
-      
-       <h2></h2>
-      {allList.map((e, i) => (
-        <section key={i}>
-          <div>{e.title}</div>
-          <CustomList type={e.type} list={e.list} />
-        </section>
-      ))}  
-
-      <h3>esercizio3</h3>
-      {allList.map((e, i) => (
-        <section key={i}>
-          <input type="text" 
-          value={listParty}
-          onChange={(e) => setListParty(e.target.value)}/>
-          <button onClick={()=>
-           setListParty([
-            ...allList,])}>aggiungi</button>
-          <div>{e.title}</div>
-         
-        </section>
+      {/* Iterazione attraverso ciascun oggetto in allLists */}
+      {allLists.map((list, index) => (
+        <div key={index}>
+          {/* Mostra il titolo basato sul valore 'title' di ciascun oggetto */}
+          <h2>{list.title}</h2>
+          {/* Utilizza il componente CustomList con type e list dai dati di ciascun oggetto */}
+          <CustomList type={list.type} list={list.list} />
+        </div>
       ))}
+      <div>
+        <input
+          type="text"
+          value={
+            inputValue
+          } /* dobbiamo gestire il valore dell input, con lo stato 'inputValue' */
+          onChange={(e) => {
+            setInputValue(
+              e.target.value
+            ); /*Aggiorna lo stato 'inputValue' con il valore dell'input  */
+          }}
+        />
+        {/* bottone per aggiungere valore alla lista */}
+        <button
+          onClick={() => {
+            setListaSeparata([
+              inputValue /* aggiunge il valore dell' input alla lista */,
+              ...listaSeparata,
+            ]);
+            setInputValue("");
+          }}
+        >
+          Aggiungi
+        </button>
+      </div>
+      <ul>
+        {listaSeparata.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+      <ul>
 
+        {listaSeparata.map((invitati) => (
+          /*Crea un elemento di lista per ciascun invitato */
+          <li key={invitati}>
+            <strong>{invitati}</strong> {/*  Visualizza il nome dell'invitato in grassetto */ }
+            <button onClick={() => SalutaInvitato(invitati)}>saluta</button>
+          </li>
+        ))}
+      </ul>
+      <p>{msg}</p>
     </div>
   );
 }
--- src/componnets/CutomList.jsx
+++ src/componnets/CutomList.jsx
@@ -1,4 +1,27 @@
-export default function ({ type, list }) {
-    const lista = list.map((el, indx) => <li key={indx}>{el}</li>);
-    return type === "ul" ? <ul>{lista}</ul> : <ol>{lista}</ol>;
+const CustomList = ({ type, list }) => {
+  /* // Verifica del tipo di lista fornito (ul o ol) e rendering della lista corrispondente */
+  
+  if (type === 'ul') {
+    /* Se il tipo è "ul", restituisce una lista non ordinata (ul)*/
+    return (
+      <ul>
+     {/*     Mapping degli elementi dell'array list per creare gli elementi della lista  */}
+        {list.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul> /*  Crea un list item per ciascun elemento nell'array */
+    );
+  } else if (type === 'ol') {
+    return (
+      <ol>
+        {list.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ol>
+    );
+  } else {
+   
   }
+};
+
+export default CustomList;
